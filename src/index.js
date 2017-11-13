8296ae0265581dd32e262743ddf609aa4963aef7
James Thompson <astrojams1@Astro-Pro.local>
2017-11-13 14:54:51 -0800

    wonky mvp of layout

--- src/index.js
+++ src/index.js
@@ -29,11 +29,13 @@ class Node extends React.Component {
   }
 
   render() {
+    var x_pos = (50 + this.props.node.xyt[0]).toString() + '%';
+    var y_pos = (50 + this.props.node.xyt[1]).toString() + '%';
     if (!this.props.node.nixed) {
       return (
-        <div className={"node depth-"+Math.min(3,this.props.node.depth)}>
-          <p ref="newText" onBlur={this.save} contentEditable="true">{this.props.node.text}</p>
-          <button onClick={this.add}>Add</button>
+        <div className={"node depth-"+Math.min(3,this.props.node.depth)} style={{left:x_pos, top:y_pos}}>
+            <p ref="newText" onBlur={this.save} contentEditable="true">{this.props.node.text}</p>
+            <button onClick={this.add}>Add</button>
         </div>
       );
     } else {
@@ -45,22 +47,59 @@ class Node extends React.Component {
 class Cluster extends React.Component {
   constructor(props) {
     super(props);
-    this.updateNodeText = this.updateNodeText.bind(this);
     this.addNode = this.addNode.bind(this);
     this.nixNode = this.nixNode.bind(this);
     this.eachNode = this.eachNode.bind(this);
+    this.updateNodeText = this.updateNodeText.bind(this);
     this.state = {
       nodes: [
         {
           depth: 0,
-          text: 'The seed of a great idea ready to explode',
+          text: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
           nixed: false,
           parent_index: null,
           sibling_index: 0,
           sub_nodes: Array(0),
+          xyt: [0,0,0],
         },
-      ]
+      ],
+      layout: 
+      {
+        d1_offset: [20,20],
+        d2_offset: [32,32],
+        d3_offset: [40,40],
+        d1_max: 10,
+        d2_max: 22,
+        d3_max: 10,
+      }
     }
+  }
+
+  calculateNodePosition(i) {
+    console.log("calculating sub-node position of " + i);
+    var arr = this.state.nodes;
+    var d1_offset = this.state.layout.d1_offset;
+    var d2_offset = this.state.layout.d2_offset;
+    console.log(arr[i].parent_index == null);
+    if (arr[i].parent_index == null) { // check if parent is the seed
+      console.log("parent is seed!")
+      var t = arr[i].sub_nodes.length * (1/this.state.layout.d1_max) * Math.PI * 2;
+      var x = d1_offset[0] * Math.cos(t);
+      var y = d1_offset[1] * Math.sin(t);
+    } else {
+      console.log("parent is not seed!")
+      var t_step = 1/this.state.layout.d2_max * Math.PI * 2;
+      var t_parent = arr[i].xyt[2];
+      var t_start = t_parent - (1 * t_step);
+      var t = t_start + t_step * arr[i].sub_nodes.length;
+      var x = d2_offset[0] * Math.cos(t);
+      var y = d2_offset[1] * Math.sin(t);
+    }
+    console.log(arr[i]);
+    console.log(x);
+    console.log(y);
+    console.log(t);
+    return [x,y,t];
   }
 
   addNode(i) {
@@ -69,23 +108,20 @@ class Cluster extends React.Component {
     // add the node to the array of nodes
     var newNode = {
       depth: arr[i].depth + 1,
-      text: '',
+      text: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
       nixed: false,
       parent_index: i,
       sibling_index: arr[i].sub_nodes.length,
       sub_nodes: Array(0),
+      xyt: this.calculateNodePosition(i),
     }
-    // add a new node to the array
-    arr.push(newNode);
-    // update the parent node with new child (which node the user clicked "add" from)
-    arr[i].sub_nodes.push(newNode);
-    // update the parents upstream with new node data until you hit the seed
+    // update tree:
+    arr.push(newNode); //add new node node to array
+    arr[i].sub_nodes.push(newNode); // update parent node with new child.
     var sub_cluster = arr[i];
     var sub_cluster_parent_index = sub_cluster.parent_index;
-    //loop until i reach the seed, whose parents index is null.
-    while (sub_cluster_parent_index) {
+    while (sub_cluster_parent_index) { // update upstream sub-clusters
       arr[sub_cluster_parent_index].sub_nodes[sub_cluster.sibling_index] = sub_cluster;
-      //set up for the next round:
       sub_cluster = arr[sub_cluster_parent_index];
       sub_cluster_parent_index = sub_cluster.parent_index;
     }
